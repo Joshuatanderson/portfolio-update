00bbae2fdd2ff9cd74430a7c74061c3b20e66c23
Joshua <joshuatanderson1@gmail.com>
2019-10-09 13:39:46 -0500

    setup w/ bulma

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<section className="hero is-primary">
+			<div className="hero-body">
+				<div className="container">
+					<h1 className="title">
+						Joshua Anderson
+      				</h1>
+					<h2 className="subtitle">
+						Frontend Developer & Designer
+      				</h2>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Header
