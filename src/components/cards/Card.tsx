9073f45e35b7091005de64ebfbea347d656fdd4b
Joshua <joshuatanderson1@gmail.com>
2019-10-09 17:46:28 -0500

    descriptions for projects added

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -11,6 +11,9 @@ const Card = ({ repo }: CardProps) => {
 		<div className="card">
 			<div className="card-content card-fixed-height has-text-centered">
 				<p className="subtitle is-4">{repo['name']}</p>
+				{repo['description'] && 
+				<p className="subtitle is-6">{repo['description']}</p>
+				}
 				<a className="button is-dark" href={repo['html_url']}>view code</a>
 			</div>
 		</div>
